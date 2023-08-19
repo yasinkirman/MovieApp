0e970462581be2448d5cffb92d4a5ca76e4a122c
yasinkirman <yasinkirman1907@gmail.com>
2023-08-19 19:13:48 +0300

    commit

--- backend/index.js
+++ backend/index.js
@@ -7,8 +7,6 @@ app.get('/', (req, res) => {
   res.send('Merhaba, Express!');
 });
 
-
-
 // Sunucuyu dinlemeye başla
 app.listen(port, () => {
   console.log(`Sunucu ${port} numaralı port üzerinden çalışıyor`);
