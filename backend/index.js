f00dd44b63b77aa5e574439879d6e751826fcfe0
yasinkirman <yasinkirman1907@gmail.com>
2023-08-19 19:10:28 +0300

    index.js changed

--- backend/index.js
+++ backend/index.js
@@ -7,11 +7,6 @@ app.get('/', (req, res) => {
   res.send('Merhaba, Express!');
 });
 
-// Diğer bir route tanımı
-// app.get('/hakkimizda', (req, res) => {
-//   res.send('Hakkımızda sayfasına hoş geldiniz.');
-// });
-
 // Sunucuyu dinlemeye başla
 app.listen(port, () => {
   console.log(`Sunucu ${port} numaralı port üzerinden çalışıyor`);
