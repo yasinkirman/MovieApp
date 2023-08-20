407ca7619f15a72fae9531803a5117e3c389badb
yasinkirman <yasinkirman1907@gmail.com>
2023-08-20 11:59:52 +0300

    AuthSchema model added

---
+++ backend/models/auth.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const AuthSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: String,
+    default: new Date(),
+  },
+});
+
+export default mongoose.model("auth", AuthSchema);
