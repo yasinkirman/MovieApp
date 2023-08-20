330d8df804f51067eca8013b0af25cdf4c13699f
yasinkirman <yasinkirman1907@gmail.com>
2023-08-20 10:56:59 +0300

    db connect added

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,18 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import database from './config/database.js';
+
+dotenv.config();
 const app = express();
+// const port = process.env.port || 5000;
 const port = 3000;
 
 // Ana sayfa için route tanımı
 app.get('/', (req, res) => {
   res.send('Merhaba, Express!');
 });
+
+database();
 
 // Sunucuyu dinlemeye başla
 app.listen(port, () => {
