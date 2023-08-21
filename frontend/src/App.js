3873bdf837dfcc2296214758383131fef50026e9
yasinkirman <yasinkirman1907@gmail.com>
2023-08-21 14:43:02 +0300

    app.js updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,16 @@
+import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
+import Auth from "./pages/Auth";
+import Home from "./pages/Home";
+
 function App() {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/auth" element={<Auth />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
