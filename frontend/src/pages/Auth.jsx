84d1960d80542e7d9a8c720f5c09a84da2eec177
yasinkirman <yasinkirman1907@gmail.com>
2023-08-26 18:56:26 +0300

    sign up page updated

--- frontend/src/pages/Auth.jsx
+++ frontend/src/pages/Auth.jsx
@@ -1,21 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Auth() {
+  const { signUp, setSignUp } = useState(true);
   return (
-    <div className="w-full h-screen bg-gray-100 flex items-center justify-center fixed top-0 right-0 bottom-0 left-0 z-50 ">
-      <div className="w-1/3 bg-white shadow-xl rounded-xl">
-        <h1 className="text-center p-2">MovieApp</h1>
-        <div className="p-2">
+    <div className="w-full h-screen bg-gray-100 flex items-center justify-center fixed top-0 right-0 bottom-0 left-0 z-50">
+      <div className="w-1/3 bg-white shadow-xl rounded-xl p-3 space-y-6">
+        <h1 className="text-2xl text-center p-2 font-bold">MovieApp</h1>
+        <div className="flex flex-col space-y-3">
+          {signUp && (
+            <input
+              type="text"
+              placeholder="Username"
+              className="input-style"
+            ></input>
+          )}
           <input
             type="text"
-            placeholder="Username"
-            className="w-full p-2"
+            placeholder="Email"
+            className="input-style"
           ></input>
-          <input type="text" placeholder="Email" className="w-full p-2"></input>
           <input
             type="text"
             placeholder="Password"
-            className="w-full p-2"
+            className="input-style"
           ></input>
         </div>
         <div className="cursor-pointer hover:bg-indigo-800 w-full p-4 text-center bg-indigo-600 text-white rounded-xl">
