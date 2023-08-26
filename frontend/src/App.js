d738a0fbd0dd779cff8fe30b620c057ab5193b62
yasinkirman <yasinkirman1907@gmail.com>
2023-08-26 18:55:29 +0300

    Navbar added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
 import Auth from "./pages/Auth";
 import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div>
+      <Navbar />
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
