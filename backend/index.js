058492b6ef5e98b23e929d64867bac7956c12091
yasin.kirman <yasinkirman1907@gmail.com>
2024-04-23 16:32:27 +0300

    index.js changed

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,18 @@
 import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
 import dotenv from "dotenv";
 import database from "./config/database.js";
 
 dotenv.config();
+
 const app = express();
-// const port = process.env.port || 5000;
-const port = 5000;
+app.use(cors());
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+
+const port = process.env.PORT || 5000;
+// const port = 5000;
 
 // Ana sayfa için route tanımı
 app.get("/", (req, res) => {
