d517715056a7b4abd7c5192e34775657a67ad7db
yasin.kirman <yasinkirman1907@gmail.com>
2024-04-23 16:33:08 +0300

    auth page useState added

--- frontend/src/pages/Auth.jsx
+++ frontend/src/pages/Auth.jsx
@@ -1,7 +1,19 @@
 import React, { useState } from "react";
 
-function Auth() {
-  const { signUp, setSignUp } = useState(true);
+const Auth = () => {
+  const [signUp, setSignUp] = useState(true);
+  const [authData, setAuthData] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+
+  const onChangeFunc = (e) => {
+    setAuthData({ ...authData, [e.target.name]: e.target.value });
+  };
+
+  console.log("authData: ", authData);
+
   return (
     <div className="w-full h-screen bg-gray-100 flex items-center justify-center fixed top-0 right-0 bottom-0 left-0 z-50">
       <div className="w-1/3 bg-white shadow-xl rounded-xl p-3 space-y-6">
@@ -9,28 +21,46 @@ function Auth() {
         <div className="flex flex-col space-y-3">
           {signUp && (
             <input
+              value={authData.username}
+              name="username"
+              onChange={onChangeFunc}
               type="text"
               placeholder="Username"
               className="input-style"
             ></input>
           )}
           <input
+            value={authData.email}
+            name="email"
+            onChange={onChangeFunc}
             type="text"
             placeholder="Email"
             className="input-style"
           ></input>
           <input
+            value={authData.password}
+            name="password"
+            onChange={onChangeFunc}
             type="text"
             placeholder="Password"
             className="input-style"
           ></input>
         </div>
+        <div className="text-red-500 text-xs cursor-pointer mb-4">
+          {signUp ? (
+            <span onClick={() => setSignUp(false)}>
+              you are already have an account?
+            </span>
+          ) : (
+            <span onClick={() => setSignUp(true)}>get sign up!</span>
+          )}
+        </div>
         <div className="cursor-pointer hover:bg-indigo-800 w-full p-4 text-center bg-indigo-600 text-white rounded-xl">
-          KAYIT OL
+          {signUp ? "REGISTER" : "LOGIN"}
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default Auth;
